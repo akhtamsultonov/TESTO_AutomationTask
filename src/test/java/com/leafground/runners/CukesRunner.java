0c86d0925d5ff6fd835877bc97b95902ba116e94
akhtamsultonov <sultonov.akhtam@gmail.com>
2021-10-21 13:54:38 +0200

    CukesRunner.java class created and added

---
+++ src/test/java/com/leafground/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.leafground.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+//  *    specifying to run it with cucumber
+@RunWith(Cucumber.class)
+
+//  *    providing option for cucumber to provide
+@CucumberOptions(
+    plugin = {"json:target/cucumber.json"},     // * what kind of report you want to get and other options  | plugins = Cucumber HTML Report path here
+    features = "src/test/resources/features",   // * where is your feature file                             | features = " feature file folder path here "
+    glue = "com/imdb/step_definitions",         // * where is the code that match the steps in cucumber     |glue = " step definitions folder path here "
+    dryRun = false,         // * whether to run the actual code or just to check if you have missing steps  | dryRun = true/false
+    tags = "@imdb"          // * which scenarios or features you want to run                                | tags = @something
+                             /*     it is another cucumber option, that allows you to run certain scenarios
+                                    You are labeling the scenario that you want to run with @tag then in cukesRunner you are using same
+                                    tag to run that scenario    */
+)
+
+public class CukesRunner {
+
+}
