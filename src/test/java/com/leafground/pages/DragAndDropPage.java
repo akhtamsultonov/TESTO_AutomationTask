550bc590bf0b3404c2f8e6ef390fae8aa8b745e2
akhtamsultonov <sultonov.akhtam@gmail.com>
2021-10-21 22:25:24 +0200

    DragAndDropPage.java class created and locators added

---
+++ src/test/java/com/leafground/pages/DragAndDropPage.java
@@ -0,0 +1,17 @@
+package com.leafground.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DragAndDropPage extends BasePage{
+
+    @FindBy(xpath = "//div[@class='ui-widget-content ui-draggable ui-draggable-handle']")
+    public WebElement source;
+
+    @FindBy(xpath = "//div[@class='ui-widget-header ui-droppable']")
+    public WebElement target;
+
+    @FindBy(xpath = "//p[.='Dropped!']")
+    public WebElement Dropped;
+}
+
