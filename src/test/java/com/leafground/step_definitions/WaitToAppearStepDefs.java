c65a5b73297a13f67d12866b591229a89d1cdbeb
akhtamsultonov <sultonov.akhtam@gmail.com>
2021-10-21 23:31:43 +0200

    WaitToAppearStepDefs.java class with step definitions are created

---
+++ src/test/java/com/leafground/step_definitions/WaitToAppearStepDefs.java
@@ -0,0 +1,22 @@
+package com.leafground.step_definitions;
+
+import com.leafground.pages.WaitToAppearPage;
+import com.leafground.utilities.Driver;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitToAppearStepDefs {
+
+    WaitToAppearPage waitToAppearPage = new WaitToAppearPage();
+
+    @Then("the user should be able get the {string} text successfully")
+    public void the_user_should_be_able_get_the_text_successfully(String expectedText) {
+        new WebDriverWait(Driver.get(),5).until(ExpectedConditions.elementToBeClickable(waitToAppearPage.appearingText));
+
+        String actualText = waitToAppearPage.appearingText.getText();
+        Assert.assertEquals("Verify that user can get the expected text",expectedText,actualText);
+
+    }
+}
