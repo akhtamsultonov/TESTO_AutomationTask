074ddb2513e7a047c6b9a88aeca510a58b8a19cc
akhtamsultonov <sultonov.akhtam@gmail.com>
2021-10-21 19:22:35 +0200

    EditPage.java class created and added

---
+++ src/test/java/com/leafground/pages/EditPage.java
@@ -0,0 +1,30 @@
+package com.leafground.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditPage extends BasePage{
+
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement email;
+
+    @FindBy(xpath = "//input[@value='Append ']")
+    public WebElement append;
+
+    @FindBy(xpath = "//input[@value='TestLeaf']")
+    public WebElement text;
+
+    public WebElement getText(){
+        return text;
+    }
+
+    @FindBy(xpath = "//input[@value='Clear me!!']")
+    public WebElement clear;
+
+    @FindBy(xpath = "//div[5]//input[1]")
+    public WebElement disabled;
+
+
+
+
+}
